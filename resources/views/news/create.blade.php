001c4b7cfef85edc6ebb10496d85fe389e67db78
patrykzym <patrykzym@kuznia.com.pl>
2021-11-30 13:29:22 +0100

    change ckeditor as npm

--- resources/views/news/create.blade.php
+++ resources/views/news/create.blade.php
@@ -15,7 +15,7 @@
 
             <div class="mb-3">
                 <label for="content" class="form-label">Opis</label>
-                <textarea class="form-control" id="textEditor" rows="3" name="content" value="{{old('description')}}"></textarea>
+                <textarea class="form-control" id="editor" rows="3" name="content" value="{{old('description')}}"></textarea>
             </div>
 
             <button type="submit" class="btn btn-primary text-center">Dodaj</button>
@@ -26,9 +26,9 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
 
 @section('javascript')
-        ClassicEditor
-            .create( document.querySelector( '#textEditor' ) )
-            .catch( error => {
-                console.error( error );
-            } );
+    ClassicEditor
+        .create(document.querySelector('#editor'))
+        .catch(error => {
+        console.error(error)
+        });
 @endsection
