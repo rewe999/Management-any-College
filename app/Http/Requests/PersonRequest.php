07b1e5b4adafb73589e863865e83dd7b553d0bf4
rewe999 <54740556+rewe999@users.noreply.github.com>
2022-02-23 17:07:22 +0100

    PersonRequest.php

--- app/Http/Requests/PersonRequest.php
+++ app/Http/Requests/PersonRequest.php
@@ -36,4 +36,33 @@ class PersonRequest extends FormRequest
             'url' => 'max:50'
         ];
     }
+
+    public function messages()
+    {
+        return [
+            'title.required' => 'tytuł jest wymagany',
+            'title.min' => 'tytuł min 2 znaki',
+            'title.max' => 'tytuł max 50 znaków',
+            'name.required' => 'imię jest wymagane',
+            'name.min' => 'imię min 2 znaki',
+            'name.max' => 'imię max 30 znaków',
+            'surname.required' => 'nazwisko jest wymagane',
+            'surname.min' => 'nazwisko min 2 znaki',
+            'surname.max' => 'nazwisko max 30 znaków',
+            'section.required' => 'sekcja jest wymagana',
+            'section.min' => 'sekcja min 3 znaki',
+            'section.max' => 'sekcja max 50 znaków',
+            'position.required' => 'pozycja jest wymagana',
+            'position.min' => 'pozycja min 3 znaki',
+            'position.max' => 'pozycja max 50 znaków',
+            'gender.required' => 'płeć jest wymagana',
+            'gender.min' => 'płeć min 5 znaków',
+            'gender.max' => 'płeć max 30 znaków',
+            'birth_date.required' => 'data urodzenia jest wymagana',
+            'email.required' => 'email jest wymagany',
+            'email.min' => 'email min 5 znaki',
+            'email.max' => 'email max 150 znaków',
+            'url.max' => 'url max 150 znaków'
+        ];
+    }
 }
